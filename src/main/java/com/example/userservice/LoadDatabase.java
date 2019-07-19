816ba90ccd01ba8a2390247f98540f199919f134
arpit jain <arpitjain2402@gmail.com>
2019-07-20 01:37:14 +0530

    Load initial Data with database support

---
+++ src/main/java/com/example/userservice/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.example.userservice;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository) {
+        return args -> {
+            userRepository.save(new UserData("Bilbo Baggins", "burglar"));
+            userRepository.save(new UserData("Frodo Baggins", "thief"));
+        };
+    }
+}
