db1902c017721c782355b3de9f71ff75a0b69f1d
arpit jain <arpitjain2402@gmail.com>
2019-07-19 23:42:28 +0530

    renames the application runner

--- src/test/java/com/example/userservice/ApplicationRunnerTests.java
+++ src/test/java/com/example/userservice/ApplicationRunnerTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AssignmentApplicationTests {
+public class ApplicationRunnerTests {
 
 	@Test
 	public void contextLoads() {
