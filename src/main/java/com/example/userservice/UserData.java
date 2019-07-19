23f6812fac7dbc92aa0e7cb433e2dad4b66dd4de
arpit jain <arpitjain2402@gmail.com>
2019-07-20 01:38:42 +0530

    adds explicit getter and setter methods

--- src/main/java/com/example/userservice/UserData.java
+++ src/main/java/com/example/userservice/UserData.java
@@ -16,14 +16,31 @@ public class UserData {
     private String username;
     private String password;
 
-    UserData() {}
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    UserData() {
+    }
 
     UserData(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setId(Long id) {
+        this.id = id;
     }
 }
