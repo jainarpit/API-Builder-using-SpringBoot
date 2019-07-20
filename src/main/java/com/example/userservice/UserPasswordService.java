d9f61f2eeb41d2e554414f3e42c7182b17fa304e
arpit jain <arpitjain2402@gmail.com>
2019-07-20 19:44:43 +0530

    refactors user password service

--- src/main/java/com/example/userservice/UserPasswordService.java
+++ src/main/java/com/example/userservice/UserPasswordService.java
@@ -6,8 +6,6 @@ import net.ricecode.similarity.StringSimilarityService;
 import net.ricecode.similarity.StringSimilarityServiceImpl;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UserPasswordService {
 
@@ -20,38 +18,33 @@ public class UserPasswordService {
         this.passwordRuleValidator = passwordRuleValidator;
     }
 
-    public boolean updatePassword(UserData userData, Long id) {
-
-        if (canChangePassword(userData.getPassword(), id)) {
-            userRepository.findById(id)
-                    .map(user -> {
-                        user.setUsername(userData.getUsername());
-                        user.setPassword(userData.getPassword());
-                        return userRepository.save(user);
-                    })
-                    .orElseGet(() -> {
-                        userData.setId(id);
-                        return userRepository.save(userData);
-                    });
-            return true;
-        } else {
+    private boolean isPasswordSimilar(String oldPassword, String newPassword) {
+        SimilarityStrategy strategy = new JaroStrategy();
+        StringSimilarityService service = new StringSimilarityServiceImpl(strategy);
+        double score = service.score(oldPassword, newPassword);
+        if (score > 0.8) {
             return false;
-        }
+        } else
+            return true;
     }
 
-    private boolean canChangePassword(String password, Long id) {
-        SimilarityStrategy strategy = new JaroStrategy();
-        Optional<UserData> user = userRepository.findById(id);
-        String oldPassword = user.get().getPassword();
-        String newPassword = password;
-        StringSimilarityService service = new StringSimilarityServiceImpl(strategy);
-        double score = service.score(oldPassword,newPassword);
-        if(score>0.8)
-        {
-            return false;
-        }
+    private boolean canChangePassword(String oldPassword, String newPassword) {
+        boolean isSimilar = isPasswordSimilar(oldPassword, newPassword);
+        boolean isPasswordValid = passwordRuleValidator.isValid(newPassword);
+        return isSimilar && isPasswordValid;
+    }
 
-        return passwordRuleValidator.isValid(password);
+    public boolean changePassword(String oldPassword, String newPassword, Long id) {
+        if (canChangePassword(oldPassword, newPassword)) {
+            userRepository.findById(id).map(user -> {
+                user.setCurrentPassword(newPassword);
+                return userRepository.save(user);
+            }).orElseGet(() -> {
+                throw new RuntimeException("User Does not exist");
+            });
+            return true;
+        } else
+            return false;
     }
 }
 
